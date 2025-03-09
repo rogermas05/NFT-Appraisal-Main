8e096fbd5bd7f9512fc44462c007f09fe8254a2e
rogermas05 <rogermas14@gmail.com>
2025-03-09 01:20:41 -0800

    port

--- Backend/Ai/Sideinfo_api/sideinfo.py
+++ Backend/Ai/Sideinfo_api/sideinfo.py
@@ -129,7 +129,7 @@ def main(contract_address, token_id):
 
 # Add this at the end of the file
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8081)))
 
 
 
